8afcf133ac5ce0fa2346084ad6d55572784751f7
kiratika723 <SIAMAKU_K@silpakorn.edu>
2025-03-15 16:11:47 +0700

    final_exam620710723

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
 import 'package:flutter/material.dart';
-import 'profile_card.dart'; // นำเข้าไฟล์โปรไฟล์การ์ด
+import 'answer1.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue.shade50, // พื้นหลังสีฟ้าอ่อน
-        appBar: AppBar(
-          title: const Text(
-            "My Profile",
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.blue.shade700, // สีฟ้าเข้ม
-        ),
-        body: const Center(
-          child: ProfileCard(
-            name: "Kiratika Siamaku",
-            position: "Student",
-            email: "kiratika110744@gmail.com",
-            phone: "+66 120 6247",
-            bio: "It's never too late to start again 🚀",
-            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyNFQOW17X0kUoAzX7E5DhnusRvxFj5TKsJg&s",
-            instagram: "babiesbananaaa", // ใส่ชื่อ IG ที่ต้องการ
-          ),
-        ),
-      ),
+      home:ProfilePage(),
     );
   }
 }
